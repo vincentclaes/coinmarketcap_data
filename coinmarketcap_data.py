f2c65a70bfeea109fdc7ea4d2e9ce3f27f09be7b
vincent <vclaes1986@gmail.com>
2018-02-24 14:35:17 +0100

    add uuid and ignore db file

--- coinmarketcap_data.py
+++ coinmarketcap_data.py
@@ -2,10 +2,18 @@
 import requests
 import time
 import json
+import os
 import pandas as pd
 from datetime import datetime
 from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
 
+def _construct_db_path(file_path):
+    return 'sqlite:///{}/coinmarketcap_data.db'.format(file_path)
+
+def load_to_db(df, db_path):
+    disk_engine = create_engine(db_path)
+    df.to_sql('crypto_data', disk_engine, if_exists='append')
 
 def get_data():
 
@@ -24,16 +32,19 @@ def get_data():
 
     df = pd.DataFrame(parsed_response)
     now = datetime.now()
-    df['date'] = now.date()
+    df['date'] = str(now.date())
     df['hour'] = now.hour
     df['minute'] = now.minute
     df['uuid'] = int(time.time())
 
     file_path = 'Users/vincent/Workspace/coinmarketcap_data'
-    db_path = 'sqlite:///{}/coinmarketcap_data.db'.format(file_path)
+    db_path = _construct_db_path(file_path)
     print '{}'.format(db_path)
-    disk_engine = create_engine(db_path)
-    df.to_sql('crypto_data', disk_engine, if_exists='append')
+    try:
+        load_to_db(df, db_path)
+    except OperationalError:
+        db_path = _construct_db_path(os.path.dirname(os.path.realpath(__file__)))
+        load_to_db(df, db_path)
 
 
 if __name__ == '__main__':
